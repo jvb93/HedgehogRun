3e7cb41633da7523cfbb085aef4ed9e550f3b730
JVB <1976577+jvb93@users.noreply.github.com>
2018-01-01 19:05:47 -0800

    beginning hoglog controller

---
+++ Site/Controllers/HogLogController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using HedgehogRun.Entities;
+using HedgehogRun.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HedgehogRun.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/HogLog")]
+    public class HogLogController : Controller
+    {
+        public ActionResult Get()
+        {
+            JObject toReturn = new JObject();
+            toReturn.Add("Test", "Hello, World!");
+            return Content(toReturn.ToString(), MimeTypeHelper.ApplicationJson);
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+               var content = reader.ReadToEnd();
+                var log = JsonConvert.DeserializeObject<HogLog>(content);
+                return Content(JsonConvert.SerializeObject(log), MimeTypeHelper.ApplicationJson);
+
+            }
+
+        }
+    }
+}
---
+++ Site/Entities/HogLog.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HedgehogRun.Entities
+{
+    public class HogLog 
+    {
+        public long Id { get; set; }
+        public int Ticks { get; set; }
+        public double TemperatureF { get; set; }
+        public double Humidity { get; set; }
+    }
+}
