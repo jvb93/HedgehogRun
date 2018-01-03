2ebda8e45ce359e0eaa2e888a1cfa9018c9690f1
JVB <1976577+jvb93@users.noreply.github.com>
2018-01-03 14:32:23 -0800

    radial gauge

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,107 +1,72 @@
-/* DHTServer - ESP8266 Webserver with a DHT sensor as an input
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h";
+#include "Secrets.h";
+#include "config.h";
 
-   Based on ESP8266Webserver, DHTexample, and BlinkWithoutDelay (thank you)
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
-   Version 1.0  5/3/2014  Version 1.0   Mike Barela for Adafruit Industries
-*/
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include "Secrets.h"
-#include <ESP8266WebServer.h>
-#include <DHT.h>
-#define DHTTYPE DHT11
-#define DHTPIN  1
+int count = 0;
 
-const char* ssid     = SSID;
-const char* password = PASSWORD;
+// set up the 'counter' feed
+// set up the 'temperature' and 'humidity' feeds
+AdafruitIO_Feed *temperature = io.feed("temperature");
+AdafruitIO_Feed *humidity = io.feed("humidity");
 
-ESP8266WebServer server(80);
- 
-// Initialize DHT sensor 
-// NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
-// you need to increase the threshold for cycle counts considered a 1 or 0.
-// You can do this by passing a 3rd parameter for this threshold.  It's a bit
-// of fiddling to find the right value, but in general the faster the CPU the
-// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
-// Arduino Due that runs at 84mhz a value of 30 works.
-// This is for the ESP8266 processor on ESP-01 
-DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
- 
-float humidity, temp_f;  // Values read from sensor
-String webString="";     // String to display
-// Generally, you should use "unsigned long" for variables that hold time
-unsigned long previousMillis = 0;        // will store last temp was read
-const long interval = 2000;              // interval at which to read sensor
- 
-void handle_root() {
-  server.send(200, "text/plain", "Hello from the weather esp8266, read from /temp or /humidity");
-  delay(100);
-}
- 
-void setup(void)
+
+void setup()
 {
-  // You can open the Arduino IDE Serial Monitor window to see what the code is doing
-  Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
-  dht.begin();           // initialize temperature sensor
+  Serial.begin(9600);
+  if (! sht31.begin(0x44))
+  {
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+  Serial.print("Connecting to Adafruit IO");
 
-  // Connect to WiFi network
-  WiFi.begin(ssid, password);
-  Serial.print("\n\r \n\rWorking to connect");
+  // connect to io.adafruit.com
+  io.connect();
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  // wait for a connection
+  while (io.status() < AIO_CONNECTED) {
     Serial.print(".");
+    delay(500);
   }
-  Serial.println("");
-  Serial.println("DHT Weather Reading Server");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-   
-  server.on("/", handle_root);
-  
-  server.on("/temp", [](){  // if you add this subdirectory to your webserver call, you get text below :)
-    gettemperature();       // read sensor
-    webString="Temperature: "+String((int)temp_f)+" F";   // Arduino has a hard time with float to string
-    server.send(200, "text/plain", webString);            // send to someones browser when asked
-  });
 
-  server.on("/humidity", [](){  // if you add this subdirectory to your webserver call, you get text below :)
-    gettemperature();           // read sensor
-    webString="Humidity: "+String((int)humidity)+"%";
-    server.send(200, "text/plain", webString);               // send to someones browser when asked
-  });
-  
-  server.begin();
-  Serial.println("HTTP server started");
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
 }
- 
-void loop(void)
+
+
+void loop()
 {
-  server.handleClient();
-} 
+  io.run();
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
 
-void gettemperature() {
-  // Wait at least 2 seconds seconds between measurements.
-  // if the difference between the current time and last time you read
-  // the sensor is bigger than the interval you set, read the sensor
-  // Works better than delay for things happening elsewhere also
-  unsigned long currentMillis = millis();
- 
-  if(currentMillis - previousMillis >= interval) {
-    // save the last time you read the sensor 
-    previousMillis = currentMillis;   
+  if (! isnan(t))
+  {
+    Serial.print("Temp *C = "); Serial.println(t);
+    temperature->save(t);
 
-    // Reading temperature for humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-    humidity = dht.readHumidity();          // Read humidity (percent)
-    temp_f = dht.readTemperature(true);     // Read temperature as Fahrenheit
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(humidity) || isnan(temp_f)) {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
   }
+  else
+  {
+    Serial.println("Failed to read temperature");
+  }
+
+  if (! isnan(h))
+  {
+    Serial.print("Hum. % = "); Serial.println(h);
+    humidity->save(h);
+  }
+  else
+  {
+    Serial.println("Failed to read humidity");
+  }
+  Serial.println();
+  delay(5000);
 }
+
+
---
+++ ArduinoCode/config.h
@@ -0,0 +1,3 @@
+#include "Secrets.h";
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
