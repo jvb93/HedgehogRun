03e27c2422571736475e752b05d580de06ca0b00
JVB <1976577+jvb93@users.noreply.github.com>
2018-01-10 18:58:01 -0800

    [arduino] added reboot if wifi is lost, code cleanup

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -9,26 +9,24 @@
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 HTTPClient http;
 
-// set up the 'counter' feed
-// set up the 'temperature' and 'humidity' feeds
-//AdafruitIO_Feed *temperature = io.feed("temperature");
-//AdafruitIO_Feed *humidity = io.feed("humidity");
-
-
-const char* host = "http://hedgehog.run/api/hoglog";
+const char* host = HOST;
 const char* ssid     = SSID;
 const char* password = WIFI_PASS;
 const byte interruptPin = 0;
-volatile int interruptCounter = 0;
+volatile int tickCount = 0;
+
 void setup()
 {
   Serial.begin(9600);
+  
+  //try to connect to temp sensor
   if (! sht31.begin(0x44))
   {
     Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
+    ESP.reset();//reboot, try again
   }
 
+  //setup hall effect sensor
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, RISING);
   
@@ -36,13 +34,15 @@ void setup()
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
+  //connect to wifi
   WiFi.begin(ssid, password);
 
- while (WiFi.status() != WL_CONNECTED) {
+  //wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -52,56 +52,50 @@ void setup()
 
 void loop()
 {
+  //if wifi was disconnected, just reboot
+  if (WiFi.status() == 6)
+  {
+    ESP.reset();
+  }
+  //get temperature and humidity
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
 
-  if (! isnan(t))
+  //make sure temp and humidity exist
+  if (! isnan(t) && ! isnan(h))
   {
-    Serial.print("Temp *C = "); Serial.println(t);
+    //build JSON payload
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    root["TemperatureF"] = (t * 9.0) / 5.0 + 32;;
+    root["Ticks"] = tickCount;
+    root["Humidity"] = h;  
+    String data = "";
+    root.printTo(data);
+    Serial.print(data);
+    
+    Serial.println("Requesting POST: ");
+    //start http connection
+    http.begin(host);
+    http.addHeader("Content-Type", "application/json");
+    //send data to server
+    int httpCode = http.POST(data);
+    String payload = http.getString();                
+    
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+    
+    http.end();  //Close connection
   }
-  else
-  {
-    Serial.println("Failed to read temperature");
-  }
 
-  if (! isnan(h))
-  {
-    Serial.print("Hum. % = "); Serial.println(h);
-  }
-  else
-  {
-    Serial.println("Failed to read humidity");
-  }
-  
- 
-
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  root["TemperatureF"] = (t * 9.0) / 5.0 + 32;;
-  root["Ticks"] = interruptCounter;
-  root["Humidity"] = h;
-
-
-   String data = "";
-   root.printTo(data);
-   Serial.print(data);
-   Serial.println("Requesting POST: ");
-  http.begin(host);
-  http.addHeader("Content-Type", "application/json");
-  int httpCode = http.POST(data);
-   String payload = http.getString();                  //Get the response payload
- 
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();  //Close connection
-   interruptCounter = 0;
-
- 
+  //reset tick count 
+  tickCount = 0; 
+  //wait one minute
   delay(60000);
 }
 
+//handler for when hall effect is triggered, add one to the count of ticks per this period
 void handleInterrupt() {
-  interruptCounter++;
+  tickCount++;
 }
 
