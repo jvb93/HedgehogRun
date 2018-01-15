c3d6bc6a03c2ee16875f81f32925b0da994ca38d
JVB <1976577+jvb93@users.noreply.github.com>
2018-01-14 19:40:48 -0800

    removed glyphicons for font awesome

--- Site/ClientApp/routes.js
+++ Site/ClientApp/routes.js
@@ -4,8 +4,8 @@ import HomePage from 'components/home-page'
 import Records from 'components/records'
 
 export const routes = [
-    { path: '/', component: Dashboard, display: 'Home', style: 'glyphicon glyphicon-home' },
-    { path: '/records', component: Records, display: 'Records', style: 'glyphicon glyphicon-star' },
+    { path: '/', component: Dashboard, display: 'Home', style: 'fa fa-home' },
+    { path: '/records', component: Records, display: 'Records', style: 'fa fa-trophy' },
    // { path: '/counter', component: CounterExample, display: 'Counter', style: 'glyphicon glyphicon-education' },
  //   { path: '/fetch-data', component: FetchData, display: 'Fetch data', style: 'glyphicon glyphicon-th-list' }
 ]
